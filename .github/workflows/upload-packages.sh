faa0b12ea09a9e284730405d0d6dc6b476995b95
hirnimeshrampuresoftware <71631645+hirnimeshrampuresoftware@users.noreply.github.com>
2022-01-25 11:53:25 +0530

    Update upload-packages.sh

--- .github/workflows/upload-packages.sh
+++ .github/workflows/upload-packages.sh
@@ -18,7 +18,14 @@ set -e
     if [ `uname -m` == "aarch64" ]; then
        pwd
        pyabis=$(echo $PYABI | tr ":" "\n")
-       /ws/.github/workflows/build-wheels.sh 
+       #/ws/.github/workflows/build-wheels.sh
+       for abi in $pyabis; do
+         find . -name *.so -delete
+         export PYABI=$abi
+         /ws/.github/workflows/build-wheels.sh
+         sudo chown -R $(id -u):$(id -g) ./*
+         mv dist/*.whl /tmp
+       done         
     else
        docker pull $DOCKER_IMAGE
        pyabis=$(echo $PYABI | tr ":" "\n")
